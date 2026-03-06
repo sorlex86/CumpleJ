b3fd741064765baad8e7984f9c7ca526e57cda12
sorlex86 <sorlex89@gmail.com>
2026-03-05 19:46:05 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+let intentos = 3
+
+function verificarRespuesta(){
+
+let r=document.getElementById("respuesta").value
+
+if(r==3){
+location.href="inicio.html"
+}else{
+document.getElementById("error").innerText="Respuesta incorrecta"
+}
+
+}
+
+function checkPassword(){
+
+const correctPassword="Anima affine"
+
+let input=document.getElementById("password").value
+
+if(input===correctPassword){
+
+location.href="app.html"
+
+}else{
+
+intentos--
+
+document.getElementById("error").innerText="Incorrecto. Intentos restantes: "+intentos
+
+if(intentos<=0){
+location.href="inicio.html"
+}
+
+}
+
+}
+
+function abrirRegalo(){
+
+let box=document.querySelector(".gift-box")
+
+box.classList.toggle("open")
+
+}
